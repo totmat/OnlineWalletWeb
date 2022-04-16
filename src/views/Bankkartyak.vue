aeab995599e2936466f442ef782b84c0d2b9d5fe
DESKTOP-6IGARIO\aradi <aradi.tams2@gmail.com>
2022-04-16 14:24:24 +0200

    Bankartya,felhasznalok,card-design+animaciok még post nincs,de van input hozzá

--- src/views/Bankkartyak.vue
+++ src/views/Bankkartyak.vue
@@ -1,13 +1,109 @@
 <template>
   <h1>Bankkártya adatok</h1>
-  <div class="row">
-      <div class="card col-lg-3 col-md-4" v-for="a in adatok" :key="a.id"> 
-          <div class="card-body" >
-              <h2>{{a.kartyanev}}</h2>
-              <h3>{{a.kartyaszam}}</h3>
-          </div>
+  
+
+<div class="container">
+  <div class="row text-center">
+    <div class="col-md-4 card-container">
+      <div class="card card-flip">
+        <div class="front card-block">
+          <!-- To add FontAwesome Icons use Unicode characters and to set size use font-size instead of fa-*x because when calculating the height (see js), the size of the icon is not calculated if using classes -->
+          <span class="card-img-top fa" style="font-size: 4em">
+              </span>
+         
+          <h4 class="card-title">Kártyák számai: <br>
+          
+          </h4>
+           
+          <h6 class="card-subtitle text-muted"><img src="../assets/creditcard_icon.png" height="250px"></h6>
+          
+        </div>
+        <div class="back card-block"  v-for="a in adatok" :key="a.id">
+            <h3>{{a.kartyaszam}}</h3>
+            <h3>{{a.kartyanev}}</h3>
+         
+        </div>
       </div>
+    </div>
+    <div class="col-md-4 card-container">
+      <div class="card card-flip">
+        <div class="front card-block">
+          <h4 class="card-title">Kártya további adatai
+              
+          </h4>
+          <h6 class="card-subtitle text-muted"><img src="../assets/creditcardback_icon.png" height="250px" alt=""></h6>
+          <p class="card-text"></p>
+        </div>
+        <div class="back card-block" v-for="a in adatok" :key="a.id">
+            <h3 >Kártya Pin</h3>
+            <h3>{{a.kartya_pin}}</h3>
+           
+
+
+  
+         
+        </div>
+      </div>
+    </div>
+ <div class="col-md-4 card-container">
+      <div class="card card-flip">
+        <div class="front card-block">
+          <h4 class="card-title">További adatok hozzáadása</h4>
+          <h6 class="card-subtitle text-muted">Front Sub-title</h6>
+          <p class="card-text"><img src="../assets/plus_icon.png" height="250px" alt=""></p>
+        </div>
+        <div class="back card-block">
+          <div class="container">
+<div class="row ">
+    <div class="col-xs-2">
+        <div class="box">
+            <form>
+                <div class="login">
+                    <label>Név</label><br>
+                    <input type="text" >
+                    <br>
+                    <label >Felhasznaló neve</label>
+                    <input type="text">
+                    <br>
+                    <label >jelszó</label>
+                    <br>
+                    <input type="text" >
+                    <br>
+                    <label>email</label>
+                    <br>
+                    <input type="text" >
+                </div>
+            </form>
+        </div>
+    </div>
+  
+</div>
+     
+ </div>
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 card-container">
+      <div class="card card-flip">
+        <div class="front card-block">
+          <span class="card-img-top fa" style="font-size: 4em">&#xf118;</span>
+          <h4 class="card-title">Front Title</h4>
+          <h6 class="card-subtitle text-muted">Front Sub-title</h6>
+          <p class="card-text">Front Text</p>
+        </div>
+        <div class="back card-block">
+          <p>Some example text</p>
+          <a href="#" class="btn btn-outline-primary">Read More</a>
+        </div>
+      </div>
+    </div>
+   
+   
+   
   </div>
+</div>
 </template>
 
 <script>
@@ -30,5 +126,71 @@ created()
 </script>
 
 <style>
+ 
+.login{  
+        width: 400px;  
+        overflow: hidden; 
+        margin: 10px 10px 10px 20px;  
+        padding: 20px;  
+        background: #919fbd;  
+        border-radius: 15px ;
+        border: solid 5px #4975d3;
 
+          
+}  
+ 
+/* for demo purposes */
+body {
+  background: rgb(245, 243, 243);
+}
+
+.card-block .btn-outline-primary {
+  width: 100%;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+}
+
+.card {
+  margin: 20px 0;
+}
+
+/* Flip Cards CSS */
+.card-container {
+  perspective: 700px;
+}
+.card-flip {
+  position: relative;
+  width: 100%;
+  transform-style: preserve-3d;
+  height: auto;
+  transition: all 0.9s ease-out;
+  background: rgb(143, 175, 228);
+  border: none;
+}
+
+.card-flip div {
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
+  height: 100%;
+  width: 100%;
+  border: none;
+}
+
+.card-flip .front {
+  position: relative;
+  z-index: 1;
+}
+
+.card-flip .back {
+  position: relative;
+  z-index: 0;
+  transform: rotateY(-180deg);
+}
+
+.card-container:hover .card-flip {
+  transform: rotateY(180deg);
+}
 </style>
--- src/views/Felhasznalok.vue
+++ src/views/Felhasznalok.vue
@@ -1,25 +1,53 @@
 <template>
   <h1>adatok</h1>
+<div class="container">
+<div class="row ">
+    <div class="col-xs-2">
+        <div class="box">
+            <form>
+                <div class="login">
+                    <label>Név</label><br>
+                    <input type="text" >
+                    <br>
+                    <label >Felhasznaló neve</label>
+                    <input type="text">
+                    <br>
+                    <label >jelszó</label>
+                    <br>
+                    <input type="text" >
+                    <br>
+                    <label>email</label>
+                    <br>
+                    <input type="text" >
+                </div>
+            </form>
+        </div>
+    </div>
+  
+</div>
+     
+ </div>
+ <div class="container">
   <div class="row">
-      
-  <div class="card col-lg-4 col-md-3" v-for="a in adatok" :key="a.id">
-      <div class="card-body">
-          <h2>{{a.nev}}</h2>
-          <h3>email : <br>{{a.email}}</h3>
-          <h3> felhasznalo neve: {{a.username}}</h3>
-               
-                  
-          <button type="button" @click="deleteFelhasznalo(a.nev)" class="btn btn-primary">Törlés</button>
-       <div class="form-group">
-            <input class="btn btn-primary" type="submit" value="Küldés" >
-        
-        </div>
-        <div>
-            
-        </div>
+    <div class="col-md-4">
+      <div class="card card-1" v-for="a in adatok" :key="a.id">
+        <h3>{{a.nev}}</h3>
       </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card card-2" v-for="a in adatok" :key="a.id">
+        <h3>{{a.username}}</h3>
+      
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card card-3"  v-for="a in adatok" :key="a.id">
+        <h3>{{a.email}}</h3>
+        
+      </div>
+    </div>
   </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -61,5 +89,96 @@ export default {
 
 <style>
 
+.container {
+  display: flex;
+  gap: 10px;
+  gap: 10px 20px; /* row-gap column gap */
+  row-gap: 10px;
+  column-gap: 20px;
+}
+.card-body{
+    background-color:aliceblue;
+   /* background-image: url("../assets/person_icon.png");*/
+    min-width: 500px;
+    height: 500px;
+    text-align: right;
+}
+.card
+{
+    display: flex;
+    
+}
+.login{  
+        width: 400px;  
+        overflow: hidden; 
+        margin: 10px 10px 10px 20px;  
+        padding: 20px;  
+        background: #919fbd;  
+        border-radius: 15px ;
+        border: solid 5px #4975d3;
 
+          
+}  
+.btn 
+{
+    
+    width:300px;
+    background-color: #1a4fc2;
+}
+
+body{
+  font-family: 'Nunito', sans-serif;
+  padding: 50px;
+}
+.card{
+    border-radius: 4px;
+    background: #fff;
+    box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
+      transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+  padding: 14px 80px 18px 36px;
+  cursor: pointer;
+}
+
+.card:hover{
+     transform: scale(1.5);
+  box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+}
+
+.card h3{
+  font-weight: 600;
+}
+
+.card img{
+  position: absolute;
+  top: 20px;
+  right: 15px;
+  max-height: 120px;
+}
+
+.card-1{
+  background-image: url(https://ionicframework.com/img/getting-started/ionic-native-card.png);
+      background-repeat: no-repeat;
+    background-position: right;
+    background-size: 80px;
+}
+
+.card-2{
+   background-image: url(https://ionicframework.com/img/getting-started/components-card.png);
+      background-repeat: no-repeat;
+    background-position: right;
+    background-size: 80px;
+}
+
+.card-3{
+   background-image: url(https://ionicframework.com/img/getting-started/theming-card.png);
+      background-repeat: no-repeat;
+    background-position: right;
+    background-size: 80px;
+}
+
+@media(max-width: 990px){
+  .card{
+    margin: 20px;
+  }
+} 
 </style>
