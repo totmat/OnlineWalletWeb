05cae26bf103eab26468d0a38b075c8b03681887
DESKTOP-6IGARIO\aradi <aradi.tams2@gmail.com>
2022-04-20 23:04:11 +0200

    adatbázis a vue-hoz és az apihoz

---
+++ Database/onlinewallet.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2022 at 01:32 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinewallet`
+--
+CREATE DATABASE IF NOT EXISTS `onlinewallet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `onlinewallet`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bankkartya`
+--
+
+DROP TABLE IF EXISTS `bankkartya`;
+CREATE TABLE IF NOT EXISTS `bankkartya` (
+  `cardNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `securityCode` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `firstName` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  PRIMARY KEY (`cardNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `felhasznalo`
+--
+
+DROP TABLE IF EXISTS `felhasznalo`;
+CREATE TABLE IF NOT EXISTS `felhasznalo` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `igazolvany`
+--
+
+DROP TABLE IF EXISTS `igazolvany`;
+CREATE TABLE IF NOT EXISTS `igazolvany` (
+  `doucmentId` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `year` varchar(255) NOT NULL,
+  `month` varchar(255) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  PRIMARY KEY (`doucmentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
